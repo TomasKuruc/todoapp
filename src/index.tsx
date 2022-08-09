a3f34e19eef03ecd3a84611df63be1b9756f37c4
Tomas Kuruc <tomkokuruc@gmail.com>
2022-08-09 14:43:33 +0200

    add browser router, provider and persist gate

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {PersistGate} from "redux-persist/integration/react";
+import {persistor, store} from "redux/store";
+import { Provider } from 'react-redux';
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <Suspense fallback={"loading"}>
+          <Provider store={store}>
+              <PersistGate loading={null} persistor={persistor}>
+                  <BrowserRouter>
+                    <App />
+                  </BrowserRouter>
+              </PersistGate>
+          </Provider>
+      </Suspense>
   </React.StrictMode>
 );
 
